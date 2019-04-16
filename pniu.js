946cb7c5c7fc5c8eb097c23628d71a9df07196f4
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-16 17:19:52 +0800

    Create pniu.js

---
+++ pniu.js
@@ -0,0 +1,21 @@
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://anthonython.com:9595/v/105/1926_1';
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);   
+var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://anthonython.com:9595/v/104_150/1926_1';
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);   
+document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\'/></a>");
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
+//document.writeln("<p>");
+document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/2886/c7d31722-3a5c-403d-98be-c6d7fcf715df.gif\'></a>")
+document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/2886/c7d31722-3a5c-403d-98be-c6d7fcf715df.gif\'></a>")
+//document.writeln("<p>");
+document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
