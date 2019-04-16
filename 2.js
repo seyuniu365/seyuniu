2bbeb4597003038e0cf2b3ec0bdaa460d647766a
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-16 17:17:32 +0800

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,5 @@
+document.writeln("<li><a href=\'https://www.ccyy777.com \' target=\'_blank\'><b>色欲妞1站</b></a></li>");
+document.writeln("<li><a href=\'https://www.jk256.com \' target=\'_blank\'><b>色欲妞2站</b></a></li>");
+document.writeln("<li><a href=\'https://www.pe121.com \' target=\'_blank\'><b>色欲妞3站</b></a></li>");
+document.writeln("<li><a href=\'https://www.6688sss.com/ \' target=\'_blank\'><b>色欲妞4站</b></a></li>");
+document.writeln("<li><a href=\'//seyuniu365.github.io/seyuniu3365 \' target=\'_blank\'><b>發布地址</b></a></li>");
