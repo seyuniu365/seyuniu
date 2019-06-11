e6fc3bc9dc9e4385a955bad5f5e6aef304de215f
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-06-11 17:07:10 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -1,19 +1,9 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c3bc5d96b45ae8330faf4d702444f6b5";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-(function(){
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-    }
-    else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-})();
+document.writeln("<!-- Global site tag (gtag.js) - Google Analytics -->");
+document.writeln("<script src=\'https://www.googletagmanager.com/gtag/js?id=UA-141615580-1\'></script>");
+document.writeln("<script>");
+document.writeln("  window.dataLayer = window.dataLayer || [];");
+document.writeln("  function gtag(){dataLayer.push(arguments);}");
+document.writeln("  gtag(\'js\', new Date());");
+document.writeln("");
+document.writeln("  gtag(\'config\', \'UA-141615580-1\');");
+document.writeln("</script>");
